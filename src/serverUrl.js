0eea9093f227a43cc66c404b30ee0acd5dbb5b22
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-12-03 12:28:21 -0500

    removes old comments from serverUrl

--- src/serverUrl.js
+++ src/serverUrl.js
@@ -1,6 +1,3 @@
-// this file is for development purposes of year-one-server to fetch data from a local dev server
-// warning: making calls to the dev server may create 'Access-Control-Allow-Origin' error
-
 const PORT = 8080;
 const LOCALHOST = `http://localhost:${PORT}`;
 // const HEROKU = 'https://year-one-server.herokuapp.com';
